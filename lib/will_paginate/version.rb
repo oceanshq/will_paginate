e8f8453575070d6071d5f185375ee0f7fb7cb5b2
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-09-12 22:06:26 +0100

    version bump

--- lib/will_paginate/version.rb
+++ lib/will_paginate/version.rb
@@ -1,8 +1,8 @@
 module WillPaginate #:nodoc:
   module VERSION #:nodoc:
-    MAJOR = 3
+    MAJOR = 10
     MINOR = 0
-    TINY  = 4
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
